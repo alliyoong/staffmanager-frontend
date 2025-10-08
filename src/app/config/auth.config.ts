03d54b1e337bc35d8e5250d2b4ea955b59687b69
khanh <khanhdt92@gmail.com>
2025-10-08 17:48:05 +0700

    successfully use keycloak to login

---
+++ src/app/config/auth.config.ts
@@ -0,0 +1,18 @@
+import { PassedInitialConfig } from "angular-auth-oidc-client";
+
+export const authConfig: PassedInitialConfig = {
+    config: {
+        authority: "http://localhost:8088/realms/hrmaz-realm",
+        clientId: "hrmaz-angular-client-id",
+        redirectUrl: window.location.origin,
+        postLogoutRedirectUri: window.location.origin,
+        responseType: "code",
+        scope: "openid profile offline_access",
+        silentRenew: true,
+        useRefreshToken: true,
+        renewTimeBeforeTokenExpiresInSeconds: 20,
+        customParamsAuthRequest: {
+            prompt: "login"
+        }
+    }
+}
---
+++ src/app/interceptor/keycloak-auth-interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { OidcSecurityService } from "angular-auth-oidc-client";
+
+@Injectable()
+export class KeycloakAuthInterceptor implements HttpInterceptor {
+
+  constructor(
+    private authService: OidcSecurityService,
+  ) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    this.authService.getAccessToken().subscribe(token => {
+        console.log('Interceptor - token:', token);
+        if (token) {
+            let header = `Bearer ${token}`;
+            let headers = req.headers.set('Authorization', header);
+            req = req.clone({ headers });
+            return next.handle(req);
+        }
+        return next.handle(req);
+    });
+    return next.handle(req);
+  }
+
+}
